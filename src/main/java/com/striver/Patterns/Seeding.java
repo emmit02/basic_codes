8b3392e07eaf79cadaa49f1040946eadaa877d90
Sanjeevani <sanjeevanisharma741390pal@gmail.com>
2024-02-25 15:26:17 +0530

    Patterns

---
+++ src/main/java/com/striver/Patterns/Seeding.java
@@ -0,0 +1,27 @@
+* * *
+* * 
+*
+
+import java.util.Scanner;
+public class Seeding {
+    public static void seeding(int n) 
+    {
+        for(int i=n;i>=1;i--)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+
+        }   
+    }
+
+    public static void main(String[] arg)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+
+
+    }
+}
